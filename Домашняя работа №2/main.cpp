78bf8b7f8e2877551a591292be4179c4572f1391
DM-02 <kid79ka@gmail.com>
2024-01-31 22:50:00 +0500

    Initial Commit

---
+++ Домашняя работа №2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Домашняя работа №2
+//
+//  Created by Дмитрий Сергеевич on 31.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
